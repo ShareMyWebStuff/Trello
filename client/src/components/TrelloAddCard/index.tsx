91039f4d56d35ad52342db6a706ee8ef3e64c916
Dave Ferguson <dave@harmonydata.co.uk>
2022-12-08 16:37:24 +0000

    Card now accepts description from the front end.

--- client/src/components/TrelloAddCard/index.tsx
+++ client/src/components/TrelloAddCard/index.tsx
@@ -11,6 +11,7 @@ export const TrelloAddCard: React.FC<{listId: number}> = ( {listId}) => {
     const dispatch = useAppDispatch ();
     const [isFocused, setIsFocused] = useState<boolean>(false)
     const [cardName, setCardName] = useState('')
+    const [desc, setDesc] = useState('')
 
     const saveList = async ()  => {
         try {
@@ -24,7 +25,7 @@ export const TrelloAddCard: React.FC<{listId: number}> = ( {listId}) => {
                 method: 'post',
                 baseURL: 'http://127.0.0.1:3002',
                 headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, 
-                data: JSON.stringify({listId, title: cardName, description: ''}),
+                data: JSON.stringify({listId, title: cardName, description: desc}),
                 validateStatus: function (status: number) {
                 return true
                 },
@@ -32,10 +33,11 @@ export const TrelloAddCard: React.FC<{listId: number}> = ( {listId}) => {
             console.log (createCard)
 
             if ( createCard.status === 201) {
-                dispatch(addCard( {listId, cardId: createCard.data.cardId, title: cardName, description: '' }) )
+                dispatch(addCard( {listId, cardId: createCard.data.cardId, title: cardName, description: desc }) )
 
                 setIsFocused(false)
                 setCardName('')
+                setDesc('')
             } else {
                 alert (createCard.data.listName)
             }
@@ -49,10 +51,11 @@ export const TrelloAddCard: React.FC<{listId: number}> = ( {listId}) => {
     return (<div  onClick={()=> {setIsFocused(true)}} className="TrelloAddCard-container" data-test="addList">
         <form>
             { isFocused ? <div>
-                <input autoFocus type="text" value={cardName} onChange={(e) =>setCardName (e.target.value)} ></input>
-            <div className=''>
-                <button onClick={(e) => {e.preventDefault(); saveList()}} >Add card</button>
-                <button onClick={(e) => {e.stopPropagation(); setIsFocused(false)}}  className=''>Cancel</button>
+                <input autoFocus type="text" placeholder="Enter card title"  value={cardName} onChange={(e) =>setCardName (e.target.value)} ></input>
+                <textarea rows={8} cols={100} placeholder="Enter card description" value={desc} onChange={(e) =>setDesc (e.target.value)} ></textarea>
+            <div>
+                <button className="add-btn" onClick={(e) => {e.preventDefault(); saveList()}} >Add card</button>
+                <button className="cancel-btn" onClick={(e) => {e.stopPropagation(); setIsFocused(false)}} >Cancel</button>
             </div>
             </div>:
             <span>+ Add a card</span>}
--- client/src/components/TrelloAddList/index.tsx
+++ client/src/components/TrelloAddList/index.tsx
@@ -46,10 +46,10 @@ export default function TrelloAddList ( {}) {
 
     return (<div  onClick={()=> {setIsFocused(true)}} className="TrelloAddList-container" data-test="addList">
         <form>
-            { isFocused ? <div><input autoFocus type="text" value={listName} onChange={(e) =>setListName (e.target.value)} ></input>
+            { isFocused ? <div><input autoFocus type="text" placeholder="Enter list" value={listName} onChange={(e) =>setListName (e.target.value)} ></input>
             <div className=''>
-                <button onClick={(e) => {e.preventDefault(); saveList()}} >Add list</button>
-                <button onClick={(e) => {e.stopPropagation(); setIsFocused(false)}}  className=''>Cancel</button>
+                <button className="add-btn" onClick={(e) => {e.preventDefault(); saveList()}} >Add list</button>
+                <button className="cancel-btn" onClick={(e) => {e.stopPropagation(); setIsFocused(false)}}>Cancel</button>
             </div>
             </div>:
             <span>+ Add another list</span>}
