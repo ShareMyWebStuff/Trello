082f9df66850a4f07c5341fb589e9d889729f81a
Dave Ferguson <dave@harmonydata.co.uk>
2022-12-08 15:49:13 +0000

    Added functionality to delete list when they have no cards

--- client/src/components/TrelloList/index.tsx
+++ client/src/components/TrelloList/index.tsx
@@ -1,6 +1,9 @@
-import React, {useState } from 'react'
+import React from 'react'
 import { TrelloCard } from '../TrelloCard'
 import {TrelloAddCard} from '../TrelloAddCard'
+import axios from 'axios'
+import { useAppDispatch } from '../../app/hooks';
+import {deleteList} from '../../features/board/boardSlice'
 import './TrelloList.css'
 
 interface ITrelloListProps {
@@ -18,9 +21,36 @@ interface ITrelloListProps {
 // 
 export const TrelloList: React.FC<ITrelloListProps> = ({ listId, listName, cards})  => {
 
+    const dispatch = useAppDispatch ();
+
+    // Use axios to delete a list
+    const deleteListById = async (listId: number)  => {
+        try {
+
+            const createList = await axios ({
+                url: `api/list/${listId}`,
+                method: 'delete',
+                baseURL: 'http://127.0.0.1:3002',
+                headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, 
+                data: JSON.stringify({}),
+                validateStatus: function (status: number) {
+                return true
+                },
+            })
+
+            if ( createList.status === 200) {
+                dispatch(deleteList( {listId} ))
+            } else {
+                alert ('Could not delete list.')
+            }
+        } catch (err) {
+            console.log (err)    
+        }
+
+    }
     return (
         <div className='TrelloList-container'>
-            <header>{listName}</header>
+            <header className="list-header">{listName}<span onClick={ (e) => {e.preventDefault(); deleteListById(listId)}} className={(cards.length === 0 ? '' : 'list-delete-hide ') + `list-delete-btn`}>X</span></header>
 
             {
                 cards.map ( card => {return <TrelloCard key={`${listId}-${card.cardId}`} cardId={card.cardId} title={card.title} description={card.description}/>})
